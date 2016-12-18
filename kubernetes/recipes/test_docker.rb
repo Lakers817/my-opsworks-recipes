8029c933d3505e4d24f59080c09cb2182d352665
Lakers817 <airmango817@gmail.com>
2016-12-17 19:35:36 -0500

    new file test

---
+++ kubernetes/recipes/test_docker.rb
@@ -0,0 +1,19 @@
+
+package "docker" do
+  action :install
+end
+
+package "bridge-utils" do
+  action :install
+end
+
+service "docker" do
+  action :disable
+end
+
+template "/etc/sysconfig/docker" do
+    mode "0644"
+    owner "root"
+    source "test_docker.erb"
+end
+
