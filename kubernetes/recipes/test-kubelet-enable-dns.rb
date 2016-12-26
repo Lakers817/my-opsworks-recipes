9a95c8efd3dedea801f51d0d4c35f1c1914e5cc0
Lakers817 <airmango817@gmail.com>
2016-12-26 12:42:58 -0500

    added test file for dns

---
+++ kubernetes/recipes/test-kubelet-enable-dns.rb
@@ -0,0 +1,20 @@
+# enable DNS setting in kubelet and restart 
+
+template "/etc/init.d/kubernetes-node" do
+    mode "0755"
+    owner "root"
+    source "kubernetes-node.erb"
+    variables({
+	  :master_url => node['kubernetes']['master_url'],
+      :dns_domain => node['kubernetes']['dns_domain'],
+      :dns_ip => node['kubernetes']['dns_ip']
+    })
+    notifies :restart, 'service[restart-kubernetes-node]', :immediately	
+end
+
+service "restart-kubernetes-node" do
+	service_name 'kubernetes-node'
+	action :nothing
+	supports :restart => true, :stop => true, :start => true 
+end
+
