02c558a9110e6ef4a2d11466a90a6f23c9166115
Lakers817 <airmango817@gmail.com>
2017-03-30 19:27:16 -0400

    new C

---
+++ kubernetes/recipes/cass-test.rb
@@ -0,0 +1,31 @@
+['data', 'commitlog', 'saved_caches'].each do |type|
+  directory "/var/lib/cassandra/#{type}" do
+
+   user 'ubuntu'
+   group 'ubuntu'
+   mode '755'
+   recursive true
+   action :create
+  end
+   end
+
+["/var/log/cassandra", "/home/ubuntu/cassandra"].each do |type|
+  directory type do
+   user 'ubuntu'
+   group 'ubuntu'
+   mode '755'
+   recursive true
+   action :create
+   end
+    end
+
+execute 'install_cassandra' do
+  user 'root'
+  cwd '/tmp'
+  command <<-EOH
+  curl -OL http://www.apache.org/dist/cassandra/3.0.12/apache-cassandra-3.0.12-bin.tar.gz
+  tar -xzvf apache-cassandra-3.0.12-bin.tar.gz
+  mv apache-cassandra-3.0.12  ~/cassandra
+  EOH
+   end
+
