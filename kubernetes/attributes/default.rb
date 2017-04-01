ac8def2464ea721678b946078cd032613d1ecbbf
Lakers817 <airmango817@gmail.com>
2017-04-01 16:53:30 -0400

    added att

---
+++ kubernetes/attributes/default.rb
@@ -0,0 +1,5 @@
+default['cassandra']['path'] = '/home/ubuntu/cassandra/apache-cassandra-3.0.12/conf'
+default['cassandra']['rack'] = 'rack2'
+default['cassandra']['data_center'] = 'dc2'
+default['cassandra']['seed_node'] = '0.0.0.0'
+
