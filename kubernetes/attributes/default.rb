90f86a2b452cdaf7400f25c39efbb86d83d828d3
Lakers817 <airmango817@gmail.com>
2017-04-02 17:38:52 -0400

    add

--- kubernetes/attributes/default.rb
+++ kubernetes/attributes/default.rb
@@ -1,6 +1,6 @@
 default['cassandra']['path'] = '/home/ubuntu/cassandra/apache-cassandra-3.0.12/conf'
-default['cassandra']['rack'] = 'rack2'
-default['cassandra']['data_center'] = 'dc2'
+default['cassandra']['rack'] = 'rack1'
+default['cassandra']['data_center'] = 'dc1'
 default['cassandra']['seed_node'] = '0.0.0.0'
 default['cassandra']['endpoint_snitch'] = 'GossipingPropertyFileSnitch'
 default['cassandra']['logs'] = '/var/log/cassandra'
